3db92b494c509ec6e7065be84176bfe7f0d0a0cc
BryanHdezL <bryan.hdezl2407@hotmail.com>
2023-08-15 02:27:56 -0600

    Operadores aritmeticos con Asignacion

---
+++ 02_Operadores/OperadoresConAsignacion/src/main/java/com/bryanhdezl/operadoresconasignacion/OperadoresConAsignacion.java
@@ -0,0 +1,19 @@
+/** *************************************************************************
+ *              OPERADORES ARITMETICOS CON ASIGNACION
+ ************************************************************************* */
+package com.bryanhdezl.operadoresconasignacion;
+
+/**
+ *
+ * @author bryan
+ */
+public class OperadoresConAsignacion {
+
+    public static void main(String[] args) {
+        // Variables
+        int numero = 10;
+        numero+=5;
+        // Mostramos el valor final de la variable.
+        System.out.println("Numero: " + numero);
+    }
+}
