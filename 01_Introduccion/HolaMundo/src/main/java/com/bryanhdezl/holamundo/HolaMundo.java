0444cbdc1ec8ec180cdc9523599ba95bbcec9394
BryanHdezL <bryan.hdezl2407@hotmail.com>
2023-08-14 22:36:18 -0600

    Hola Mundo en Java

---
+++ 01_Introduccion/HolaMundo/src/main/java/com/bryanhdezl/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package com.bryanhdezl.holamundo;
+
+/**
+ *
+ * @author bryan
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo, mi nombre es Bryan.");
+        System.out.println("Curso Java ATS.");
+    }
+}
