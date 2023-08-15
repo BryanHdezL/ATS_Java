937874e1a17f2828daf1c30d3c386db75ae2e370
BryanHdezL <bryan.hdezl2407@hotmail.com>
2023-08-15 00:34:32 -0600

    Constantes

---
+++ 01_Introduccion/Constantes/src/main/java/com/bryanhdezl/constantes/Constantes.java
@@ -0,0 +1,21 @@
+/** *************************************************************************
+ *                             CONSTANTES
+ *
+ * Constante: Espacio de memoria donde se almacena un valor, que no cambia
+ *            su valor.
+ ************************************************************************* */
+package com.bryanhdezl.constantes;
+
+/**
+ *
+ * @author bryan
+ */
+public class Constantes {
+
+    public static void main(String[] args) {
+        // Constante.
+        final float PI = 3.1416f;
+        // Mostramos el valor de las constantes.
+        System.out.println("Constante PI: " + PI);
+    }
+}
